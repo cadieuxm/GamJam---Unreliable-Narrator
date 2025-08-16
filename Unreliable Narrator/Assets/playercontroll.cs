c67a6bd6bce6bba9c538a01265fe0aafd7211377
Andrew <theandrewriddle@gmail.com>
2025-08-16 17:21:06 -0500

    Added jumping etc

--- Unreliable Narrator/Assets/playercontroll.cs
+++ Unreliable Narrator/Assets/playercontroll.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,6 +9,7 @@ public class playercontroll : MonoBehaviour
     float movementSpeed = 10f;
     Rigidbody2D rb;
     Vector2 horizInput, velocity, targetVelocity, currentVelocity;
+    bool canJump = true;
 
     private void Awake ()
     {
@@ -38,9 +40,41 @@ public class playercontroll : MonoBehaviour
         horizInput = move.ReadValue<Vector2>();
         velocity = new Vector2();
         targetVelocity = new Vector2(horizInput.x * movementSpeed, rb.linearVelocity.y);
-        rb.linearVelocity = Vector2.SmoothDamp(rb.linearVelocity, targetVelocity, ref currentVelocity, 1); //change later for smoothing
+        rb.linearVelocity = Vector2.SmoothDamp(rb.linearVelocity, targetVelocity, ref currentVelocity, 0.05f); //change later for smoothing
+
+        if (jump.IsPressed())
+        {
+
+            if (canJump == true)
+            {
+                rb.AddForceY(400.0f);
+            }
+            
+
+
+
+        }
     }
 
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        //check if player collision leaves contact with ground
+        if (collision.gameObject.CompareTag("Ground")){
+            canJump = false;
+            print("ungrounded,can'tjump");
+        }
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //check for collision with ground
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            print("grounded");
+            canJump = true;
+
+        }
+    }
 
 
 }
