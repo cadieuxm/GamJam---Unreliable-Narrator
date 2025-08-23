17bae45a9bc586af5c13101b1ecf633a2027995d
Andrew <theandrewriddle@gmail.com>
2025-08-23 18:47:22 -0500

    Reworked player movement, added proper wall clinging and wall jumping

--- Unreliable Narrator/Assets/InputSystem_Actions.cs
+++ Unreliable Narrator/Assets/InputSystem_Actions.cs
@@ -143,7 +143,7 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
                     ""id"": ""f1ba0d36-48eb-4cd5-b651-1c94a6531f70"",
                     ""expectedControlType"": """",
                     ""processors"": """",
-                    ""interactions"": ""Hold(duration=0.2)"",
+                    ""interactions"": """",
                     ""initialStateCheck"": false
                 },
                 {
